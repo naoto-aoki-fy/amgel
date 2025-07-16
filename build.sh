74ef76e6ea8fcdc55d52a9dd8003a768f34f24b4
Naoto Aoki <184037301+naoto-aoki-fy@users.noreply.github.com>
2025-07-16 16:38:07 +0900

    renamed to amgel.cpp

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ if [[ ./cdl86/cdl.c -nt cdl.o ]]; then
 fi
 
 if [[ ./mynccl.cpp -nt ./mynccl.so ]]; then
-  nvcc -Wno-deprecated-gpu-targets -shared -Xcompiler -fPIC -std=c++11 -lmpi -lnccl -lelf -rdc=true --cudart=shared -I./cdl86 -I./atlc/include ./mynccl.cpp cdl.o -o mynccl.so
+  nvcc -Wno-deprecated-gpu-targets -shared -Xcompiler -fPIC -std=c++11 -lmpi -lnccl -lelf -rdc=true --cudart=shared -I./cdl86 -I./atlc/include ./amgel.cpp cdl.o -o mynccl.so
 fi
